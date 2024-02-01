c4841f23b633686ef4ae8764a07d29034a8de860
nikhil-shanbhag <nikhilshanbhag35@gmail.com>
2024-02-01 20:12:10 +0530

    Add map function

--- src/App.jsx
+++ src/App.jsx
@@ -6,32 +6,41 @@ import Shoe2 from "./assets/PumaSneakers2.avif";
 import Shoe3 from "./assets/PumaSneakers3.avif";
 import Shoe4 from "./assets/PumaSneakers4.avif";
 import Footer from "./components/Footer";
+import { data } from "autoprefixer";
 
 const App = () => {
+  const datas = [
+    {
+      name: "PUMA x STAPLE G Men's Suede Sneakers",
+      price: "$100.00",
+      img: Shoe1,
+    },
+
+    {
+      name: "Suede Brand Love II Men's Sneakers",
+      price: "$95.00",
+      img: Shoe2,
+    },
+
+    {
+      name: "FENTY x PUMA Creeper Phatty Men's Sneakers",
+      price: "$75.00",
+      img: Shoe3,
+    },
+
+    {
+      name: "PUMA x SWAROVSKI Suede Women's Sneakers",
+      price: "$90.00",
+      img: Shoe4,
+    },
+  ];
   return (
     <div>
       <Header />
       <div className="hero flex justify-around mt-10">
-        <Hero
-          name="PUMA x STAPLE G Men's Suede Sneakers"
-          price="$100.00"
-          img={Shoe1}
-        />
-        <Hero
-          name="Suede Brand Love II Men's Sneakers"
-          price="$95.00"
-          img={Shoe2}
-        />
-        <Hero
-          name="FENTY x PUMA Creeper Phatty Men's Sneakers"
-          price="$75.00"
-          img={Shoe3}
-        />
-        <Hero
-          name="PUMA x SWAROVSKI Suede Women's Sneakers"
-          price="$90.00"
-          img={Shoe4}
-        />
+        {datas.map((data) => {
+          return <Hero name={data.name} price={data.price} img={data.img} />;
+        })}
       </div>
       <Footer />
     </div>
